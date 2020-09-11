53dea6668fd5d3ada29fea27530fbb510e85d0aa
ericwugo <ericwugo@gmail.com>
2020-09-12 06:10:36 +0800

    Initial Commit

---
+++ passwordUnlockEricApp1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  passwordUnlockEricApp1
+//
+//  Created by user on 2020/9/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ passwordUnlockEricApp1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  passwordUnlockEricApp1
+//
+//  Created by user on 2020/9/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
